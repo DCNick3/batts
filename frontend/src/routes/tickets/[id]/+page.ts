16a3c0f77f44fe1af07c4cd2a82a7cfaa6cc5243
Anatolay <nihonvasserman228@gmail.com>
2023-10-08 20:37:57 +0300

    Add type inference to page data

--- frontend/src/routes/tickets/[id]/+page.ts
+++ frontend/src/routes/tickets/[id]/+page.ts
@@ -1,12 +1,10 @@
-import { getTicketData } from '$lib/mocks/database'
 import type { PageLoad } from './$types'
 import { Api } from '$lib/api'
-import type { TicketView } from 'backend/bindings/TicketView'
 
-export const load: PageLoad<TicketView> = async ({ fetch, params }) => {
+export const load: PageLoad = async ({ fetch, params }) => {
   const api = new Api(fetch)
   const result = await api.getTicket(params.id)
 
   if (result.status === "Error") throw Error(result.payload.report)
-  return result.payload as TicketView
+  return result.payload
 }
