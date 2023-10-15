06e6a4e8a6bf409b5ee5c6cd1fa067f446d0db2a
Anatolay <nihonvasserman228@gmail.com>
2023-10-15 17:42:03 +0300

    Handle connection failure

--- frontend/src/routes/tickets/[id]/+page.ts
+++ frontend/src/routes/tickets/[id]/+page.ts
@@ -1,31 +1,47 @@
 import type { PageLoad } from './$types'
+import type { TicketView, ApiError } from 'backend'
 import { Api } from 'backend'
 
-export const load: PageLoad = async ({ fetch, params }) => {
+/*
+  TypeScript is when you put lots of `as` hack,
+  yet still believe you have type safety
+*/
+type Hack = { status: 'ConnectionError', ticketId: string }
+type Data
+  = { status: 'Success', payload: TicketView, users: Map<string, string>, ticketId: string }
+  | { status: 'Error', payload: ApiError, ticketId: string }
+  | Hack
+
+export const load: PageLoad<Data> = async ({ fetch, params }) => {
   const api = new Api(fetch)
-  const result = await api.getTicket(params.id)
 
-  if (result.status === "Success") {
-    const userIds = new Set<string>()
-    result.payload.timeline.forEach(item => {
-      if (item.content.type === "Message") {
-        userIds.add(item.content.from)
-      }
-    })
-    const users = new Map<string,string>()
+  try {
+    const result = await api.getTicket(params.id)
 
-    const responses = await Promise.all(Array.from(userIds).map(id => api.getUserProfile(id)))
+    if (result.status === "Success") {
+      const userIds = new Set<string>()
+      result.payload.timeline.forEach(item => {
+        if (item.content.type === "Message") {
+          userIds.add(item.content.from)
+        }
+      })
+      const users = new Map<string,string>()
 
-    responses.forEach(res => {
-      if (res.status === "Success") {
-        users.set(res.payload.id, res.payload.name) 
-      } else {
-        console.error(res.payload.report)
-      }
-    })
+      const responses = await Promise.all(Array.from(userIds).map(id => api.getUserProfile(id)))
 
-    return { users, ticketId: params.id, ...result }
+      responses.forEach(res => {
+        if (res.status === "Success") {
+          users.set(res.payload.id, res.payload.name) 
+        } else {
+          console.error(res.payload.report)
+        }
+      })
+
+      return { users, ticketId: params.id, ...result }
+    }
+
+    return { ticketId: params.id, ...result }
+  } catch (error) {
+    return { ticketId: params.id, status: 'ConnectionError'} as Hack
   }
-
-  return { ticketId: params.id, ...result }
 }
