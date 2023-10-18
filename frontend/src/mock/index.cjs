ffdae3e998ecf20584512726b1fad13aa001e2e5
Anatolay <nihonvasserman228@gmail.com>
2023-10-18 17:48:54 +0300

    Add simple mocks

---
+++ frontend/src/mock/index.cjs
@@ -0,0 +1,42 @@
+const express = require('express')
+const cors = require('cors')
+
+const router = express.Router()
+
+const PORT = 3000
+
+//@ts-ignore
+const succeed = (data, status=200) => (_req, res) => {
+    res.status(status).send({
+        status: 'Success',
+        payload: data
+    })
+}
+//@ts-ignore
+const fail = (data, status=404) => (_req, res) => {
+    res.status(status).send({
+        status: 'Fail',
+        payload: data
+    })
+}
+
+router.get('/users/me', succeed(require('./data/me.json')))
+// router.get('/users/me', fail(require('./data/me.404.json')))
+
+router.get('/users/:id/profile', succeed(require('./data/user.json')))
+// router.get('/users/:id', fail(require('./data/?')))
+
+router.get('/tickets/owned', succeed(require('./data/ownedTickets.json')))
+// router.get('/tickets/owned', fail(require('./data/?')))
+
+router.get('/tickets/assigned', succeed(require('./data/assignedTickets.json')))
+// router.get('/tickets/assigned', fail(require('./data/?')))
+
+router.get('/tickets/:id', succeed(require('./data/ticket.json')))
+// router.get('/tickets/:id', fail(require('./data/?')))
+
+const app = express()
+app.use(cors())
+app.use('/api', router)
+console.log("Started")
+app.listen(PORT)
