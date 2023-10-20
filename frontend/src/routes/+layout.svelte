7aa610637faa60c36a15aa422cb80237876caf0a
Anatolay <nihonvasserman228@gmail.com>
2023-10-20 12:03:08 +0300

    Add navbar on mobile devices

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -4,6 +4,7 @@
 	import { setContext } from 'svelte'
 	import { writable } from 'svelte/store'
 	import { Button } from 'flowbite-svelte'
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Dropdown, DropdownItem } from 'flowbite-svelte'
 	import NavLink from '$lib/components/NavLink.svelte'
 
 	import type { LayoutData } from './$types'
@@ -21,8 +22,28 @@
 	}
 </script>
 
-<div class="flex grow shrink basis-full h-screen">
-	<aside class="flex flex-col items-center w-64 bg-slate-50 gap-6 p-4">
+<div class="flex max-md:flex-col grow shrink basis-full h-screen">
+	<Navbar class="md:hidden p-4 bg-slate-50">
+		<NavBrand href="/">
+			<Logo class="w-8 h-8" />
+		</NavBrand>
+		{#if $user == null}
+			<Button
+				on:click={goToLogin}
+				class="text-sm p-1"
+			>
+				Login
+			</Button>
+		{:else}
+			<NavLink class="text-sm" href="/me">{$user.name}</NavLink>
+			<NavHamburger class="m-0" />
+			<Dropdown>
+				<DropdownItem href="/">Open a Ticket</DropdownItem>
+				<DropdownItem href="/assigned">Assigned Tickets</DropdownItem>
+			</Dropdown>
+			{/if}
+	</Navbar>
+	<aside class="max-md:hidden flex flex-col items-center w-64 bg-slate-50 gap-6 p-4">
 		<a href="/" class="block w-fit">
 			<Logo />
 		</a>
