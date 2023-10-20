c4a93ea5a13f3175c8b5c0091045812a76acea4c
Anatolay <nihonvasserman228@gmail.com>
2023-10-20 14:00:56 +0300

    Customize table for mobile

--- frontend/src/lib/components/TicketList.svelte
+++ frontend/src/lib/components/TicketList.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import {
 		Table,
+		TableHead,
+		TableHeadCell,
 		TableBody,
 		TableBodyCell,
 		TableBodyRow
@@ -12,30 +14,44 @@
 
 </script>
 
-<Table hoverable color="default" class="border rounded-md border-separate border-spacing-0">
-	<colgroup>
+<Table
+	hoverable
+	color="default"
+	class="border rounded-md border-separate border-spacing-0 flex flex-row flex-no-wrap sm:inline-table"
+	divClass=""
+>
+	<colgroup class="hidden sm:table-column-group">
 		<col class="w-16"/>
 		<col class="w-48"/>
 		<col class="w-fit"/>
 	</colgroup>
-	<TableBody tableBodyClass="rounded-sm">
+	<TableHead defaultRow={false} class="bg-white">
+		{#each tickets as _}
+			<tr class="sm:hidden sm:first:table-row flex flex-col flex-nowrap last:mb-0 mb-2 sm:mb-0 bg-gray-50">
+				<TableHeadCell class="p-2.5 sm:px-6">Status</TableHeadCell>
+				<TableHeadCell class="p-2.5 sm:px-6">Submitted To</TableHeadCell>
+				<TableHeadCell class="p-3 sm:px-6">Topic</TableHeadCell>
+			</tr>
+		{/each}
+	</TableHead>
+	<TableBody tableBodyClass="rounded-sm flex-1 sm:flex-none">
 		{#each tickets as ticket}
 		<!-- TODO: Three hardcoded false's come from the `ticket.up` property -->
-			<TableBodyRow class={"first:rounded-t-sm last:rounded-b-sm" + (true ? "" : " bg-gray-50")}>
+			<TableBodyRow class={"flex sm:table-row flex-col flex-nowrap first:rounded-t-sm last:rounded-b-sm last:mb-0 mb-2 sm:mb-0 border-b-0" + (true ? "" : " bg-gray-50")}>
 				<TableBodyCell
-					class="px-2 text-sm rounded-l-md text-center"
+					class="py-2 sm:py-4 sm:px-2 text-sm rounded-l-md sm:text-center"
 				>
 					<StatusBadge status={ticket.status} />
 				</TableBodyCell>
 				<TableBodyCell
-					class="text-sm text-slate-500"
+					class="py-2 sm:py-4 text-sm text-slate-500"
 				>
 					<a href="/">
 						{ticket.destination}
 					</a>
 				</TableBodyCell>
 				<TableBodyCell
-					class={`text-base font-${false ? 'semibold' : 'medium'} rounded-r-md`}
+					class={`py-2 sm:py-4 text-base font-${false ? 'semibold' : 'medium'} rounded-r-md`}
 				>
 					<a href={`/tickets/${ticket.id}`} class="flex">
 						{#if false}
