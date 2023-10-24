8e0afda7953f4ec7d8c847da90531aca9ff88e77
Anatolay <nihonvasserman228@gmail.com>
2023-10-24 13:53:33 +0300

    Avoid highlighting visited links

--- frontend/src/lib/components/A.svelte
+++ frontend/src/lib/components/A.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <a
-  class="visited:text-primary-700 text-primary-600 hover:text-primary-500 transition font-semibold"
+  class="visited:text-primary-700 text-primary-700 hover:text-primary-500 transition font-semibold"
   on:click
   href={href}
 >
