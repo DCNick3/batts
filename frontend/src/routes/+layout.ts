cff8416c4d4d24c6dd999f8a1be4eff802af7408
Nikita Strygin <nikita6@bk.ru>
2023-11-06 21:39:56 +0300

    Add posthog self-hosted analytics

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -1,9 +1,18 @@
 import { Api } from 'backend'
 import type { LayoutLoad } from './$types'
 import type { UserView, GroupView } from 'backend'
+import posthog from 'posthog-js'
+import { browser } from '$app/environment';
 
 export const load: LayoutLoad<{ user: UserView | null }> = async ({ fetch }) => {
   const api = new Api(fetch)
+
+  if (browser) {
+    posthog.init(
+        'phc_vANccchbK5FWoseTMHFPIHIr1h3EAKHOFOXa4dN3hBn',
+        { api_host: 'https://ph.batts.tatar' }
+    )
+  }
 
   let user: null | UserView = null
   let userGroups: GroupView[] = []
