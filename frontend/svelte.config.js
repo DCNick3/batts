659fd540718553e03ff282539e0055a0f1baf1b0
Nikita Strygin <nikita6@bk.ru>
2023-11-07 18:53:42 +0300

    Explicitly specify sveltekit assets path

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -10,7 +10,13 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
-	}
+		paths: {
+			// Work around a race condition in rrweb while capturing a session replay
+			// if the user navigates away from the page before the session is serialized, the css href will be resolved relative to an incorrect base.
+			// This will hopefully force svelte to use an absolute path for the css href
+			assets: "https://batts.tatar"
+		}
+	},
 };
 
 export default config;
